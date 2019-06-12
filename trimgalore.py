35a17c914e3c9f1167d6765d7c2cde7a92c98d37
awanalkoerdi <31916282+awanalkoerdi@users.noreply.github.com>
2019-06-12 12:53:36 +0200

    chocolade

--- trimgalore.py
+++ trimgalore.py
@@ -6,11 +6,11 @@ from Bio import SeqIO
 # print("%i reads" % count)
 
 good_reads = (rec for rec in \
-              SeqIO.parse("SRR5832186.fastq", "fastq") \
+              SeqIO.parse("SRR5832185.fastq", "fastq") \
               if min(rec.letter_annotations["phred_quality"]) >= 30)
-count = SeqIO.write(good_reads, "good_quality_SRR5832186.fastq", "fastq")
+count = SeqIO.write(good_reads, "good_quality_SRR5832185.fastq", "fastq")
 print("Saved %i reads" % count)
 
-trimmed_SRR5832185 = (rec[13:85] for rec in SeqIO.parse("good_quality_SRR5832186.fastq", "fastq"))
-count = SeqIO.write(trimmed_SRR5832185, "trimmed_SRR5832186.fasta", "fasta")
+trimmed_SRR5832185 = (rec[13:96] for rec in SeqIO.parse("good_quality_SRR5832185.fastq", "fastq"))
+count = SeqIO.write(trimmed_SRR5832185, "trimmed_SRR5832185.fastq", "fastq")
 print("Saved %i reads" % count)
