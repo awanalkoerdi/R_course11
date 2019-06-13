dacfd5b1a405d2c3e30d7948a6ac0e92c32a4254
awanalkoerdi <31916282+awanalkoerdi@users.noreply.github.com>
2019-06-13 11:08:11 +0200

    Update

--- determine differentially expressed genes.R
+++ determine differentially expressed genes.R
@@ -9,8 +9,8 @@ library(edgeR)
 library(ggplot2)
 library(data.table)
 
-#Function to create countmatrix from seperate txt files
-createCountMatrix <- function(){
+#Function to create countmatrix from seperate text files.
+create_countmatrix <- function(){
   
   #Open all count files
   count85 <- (read.csv("counts_SRR5832185.txt", header = FALSE, sep = "\t"))
@@ -24,7 +24,7 @@ createCountMatrix <- function(){
 
   #split the chromosome and gene information
   split1 <- do.call(rbind ,strsplit(as.character(count85[,1]), ":", fixed = TRUE))
-  split2 <- do.call(rbind ,strsplit(as.character(split1[,2]),  ".", fixed = TRUE))
+  split2 <- do.call(rbind ,strsplit(as.character(split1[,2]) , ".", fixed = TRUE))
 
   #Add chromosome and gene information  to count85 dataframe and removes old column
   count85 <- cbind(split2, count85)
@@ -32,16 +32,14 @@ createCountMatrix <- function(){
 
 
   #creates count matrix 
-  #countMatrix <- cbind(cbind(count85, count86$V2, count87$V2),cbind(count94$V2, count95$V2, count96$V2))
   countMatrix <- do.call(cbind, list(count85, count86$V2, count87$V2, count94$V2, count95$V2, count96$V2))
-
   colnames(countMatrix) <- c("Chromosome", "Gene", "E.coli_85", "E.coli_86", "E.coli_87", "B.subtillis_94", "B.subtillis_95", "B.subtillis_96")
 
   write.csv(countMatrix ,"countMatrix.csv", row.names = FALSE)
 }
 
 
-differentially_expressed_genes <- function(){
+differentially_expressed_genes <- function(res){
   # open file
   countMatrix <- read.csv("countMatrix.csv", header = TRUE)
   rownames(countMatrix) <- countMatrix[,"Gene"]
@@ -64,7 +62,6 @@ differentially_expressed_genes <- function(){
   # create design matrix (samples grouped by conditions)
   design <- model.matrix(~0+group, data = y$samples)
   colnames(design) <- levels(y$samples$group)
-  #print(design)
   
   # estimate dispersion
   y <- estimateGLMCommonDisp( y, design)
@@ -82,24 +79,25 @@ differentially_expressed_genes <- function(){
   mc  <- makeContrasts(exp.r=E.coli-B.subtillis, levels = design)
   fit <- glmLRT(fit, contrast = mc)
   res <- topTags(fit, n = 100000, p.value = 0.05)
-  print(res)
-  
   result_df <- as.data.frame(topTags(fit, n = 100000, p.value = 0.05))
   
   write.csv(res ,"diff_expressed_genes.csv", row.names = TRUE)
+  return(res)
+}
   
+#creates a text file out of a csv file. 
+create_gsea_input <- function(){ 
+  res <- differentially_expressed_genes(res)
   subSet <<- as.data.frame(res[,4:5])
   setDT(subSet, keep.rownames = TRUE)[]
-  
+
   write.table(subSet, "gsea_input.txt", sep = "\t", row.names = FALSE)
-  
 }
 
-
 main <- function(){
-  createCountMatrix()
-  differentially_expressed_genes()
-
+  create_countmatrix()
+  differentially_expressed_genes(res)
+  create_gsea_input()
 }
 
 main()
